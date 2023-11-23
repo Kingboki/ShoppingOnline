6ee252c95a2cf6fdc1b671e125bf0ab6939b5d8e
TienDat <128807325+Kingboki@users.noreply.github.com>
2023-11-23 08:52:14 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster01.imp2h3y.mongodb.net',
+  DB_USER: 'hotiendat',
+  DB_PASS: '123',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: '442',
+  JWT_EXPIRES: '99999999999999999', // in milliseconds
+  EMAIL_USER: 'hotiendat1112@gmail.com', // gmail service
+  EMAIL_PASS: '<password>'
+}
+module.exports = MyConstants;
